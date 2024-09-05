0ef08516fcba284b287bd210514f37af68ca95df
AlexanderStudiesInfo <axh1497@miami.edu>
2024-09-05 19:18:25 -0400

    update helloworld
    update helloworld sept 5th

--- HelloWorld/helloworld.js
+++ HelloWorld/helloworld.js
@@ -16,22 +16,67 @@ var printer = {
 const http = require('http');//Puts the required module into a contant called http
 //Define the port the app will be accesssed from (80,8080,8888 are default to the domain /)
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 8080; 
 //The HTTP module can create an HTTP server that listens to server ports and gives a response back to the client
 
-//Use the createServer() method to create an HTTP server:
+//Use the createServer() method to create an HTTP server: 
 function handlerRequest(request,response) {
     response.writeHead(200, { "Content-Type" : "Text/Plain"})
     response.end("Hello World")
 }
 
 const server = http.createServer( (request,response) => {
-    console.log(request)
-    response.writeHead(200 , { "Content-Type":'"Text/Plain"})
-    response.end("Hello World")
+    console.log("*****************")
+    console.log("*****************")
+
+    console.log(request.url)
+    console.log(request.method)
+    //GET POST PUT DELETE
+    //GET => READ OPERATION OF A DATABASE
+    //POST => CREATE "" ""
+    //PUT => UPDATE
+    //DELETE => DELETE
+
+    //How to respond to requests...
+    //ROUTING
+    if(request.url == "/") {
+        //execute the statement
+        response.writeHead(200 , { "Content-Type":"Text/Plain"})
+        response.end("Home Page")
+    }else if(request.url == "/contact") {
+        //execute the statement
+        response.writeHead(200 , { "Content-Type":"Text/Plain"})
+        response.end("Contact Page")
+    }else if(request.url == "/about") {
+        //execute the statement
+        response.writeHead(200 , { "Content-Type":"Text/Plain"})
+        response.end("About Page")
+    }else if(request.url == "/gallary") {
+        //execute the statement
+        response.writeHead(200 , { "Content-Type":"Text/HTML"})
+        response.end("<html><head><title>Page Title </title></head><body><h1>My first HTML response</h1></body></html>")
+    }else{
+        response.writeHead(400 , { "Content-Type" : "Text/Plain"})
+        response.end("Page not found error 400")
+    }
+    //Basic Conditions: 
+    /**
+     * Equals: a == b (Equals sign twice because = by itself is an assignment operator)
+     * Not Equals: if a != b 
+     * Greater than: if a > b
+     * Less than: if a < b 
+     * Greater than or equal: if a >= b
+     * Lesser than or equal: if a <= b
+     */
+
+    console.log("Respond to request")
+
+
+    console.log("*****************")
+    console.log("*****************")
 })
 
 //start the server
-server.listen(PORT, () => console.log(' server started on port ${PORT}  + press Ctrl-C to terminate' ))
+server.listen(PORT, () => console.log(' server started on port http://localhost:${PORT} press ctrl + c to terminate' ))
 
 //server.listen(PORT)
